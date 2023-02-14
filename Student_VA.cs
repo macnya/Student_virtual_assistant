d26cb3df742e234cc81da45fc178f28ff019b391
macnya <61224738+macnya@users.noreply.github.com>
2023-02-14 10:29:06 +0300

    Delete Student_VA.cs

--- Student_VA.cs
+++
@@ -1,5 +0,0 @@
-﻿Build started...
-1>------ Build started: Project: Student_VA, Configuration: Debug Any CPU ------
-1>Skipping analyzers to speed up the build. You can execute 'Build' or 'Rebuild' command to run analyzers.
-1>Student_VA -> C:\Users\USER\Desktop\New folder\Student_VA\bin\Debug\net6.0\Student_VA.dll
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
